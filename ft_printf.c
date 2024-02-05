052ba05af15f839b586560a0ae5061bdc8b22f1c
bbento-a <bbento-a@student.42lisboa.com>
2024-02-05 21:19:29 +0000

    formats flags ifs

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bbento-a <bbento-a@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 11:03:07 by bbento-a          #+#    #+#             */
-/*   Updated: 2023/11/23 16:57:18 by bbento-a         ###   ########.fr       */
+/*   Updated: 2023/12/06 17:21:16 by bbento-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,16 @@ int	ft_format_flags(va_list args, const char format)
 		counter += ft_printf_percent();
 	else if (format == 'c')
 		counter += ft_printf_c(va_arg(args, char));
+	else if (format == 's')
+		counter += ft_printf_s(va_arg(args, char));
+	else if (format == 'p')
+		counter += ft_printf_p(va_arg(args, unsigned int));
+	else if (format == 'i')
+		counter += ft_printf_i(va_arg(args, int));
+	else if (format == 'u')
+		counter += ft_printf_u(va_arg(args, unsigned int));
+	else if (format == 'x' || format == 'X')
+		counter += ft_print_xX(va_arg(args, unsigned int));
 	return (counter);
 }
 
@@ -31,7 +41,7 @@ int	ft_printf(const char *s, ...)
 	int		i;
 	va_list args;
 
-	va_start(args, count);
+	va_start(args, s);
 	counter = 0;
 	i = 0;
 	while (s[i])
